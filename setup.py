2dda60f7087a0889255a03fcb0c72a607175304f
srivanthsai <srivanthsail@gmail.com>
2024-04-07 00:44:41 +0530

    error has been takencareof

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import find_packages, setup 
-from typing import List
+from typing import List # type: ignore
 hyphen='-e .'
 def get_requirements(file_path:str)->List[str]:
     reqs=[]
