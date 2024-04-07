4a4027abd9a9abd0c0eaf99583c038aac9448ca7
srivanthsai <srivanthsail@gmail.com>
2024-04-08 00:56:03 +0530

    logger and exception

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,4 @@
 import sys
-import logging
-import os
 def custom_error_message(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info() #this gives the line no and on which file
     file_name=exc_tb.tb_frame.f_code.co_filename
